a1f3a97100f87efb4e33a244905c5738d10e338d
Dan Williams <me@deedubs.com>
2013-08-26 12:46:32 -0400

    Example modular app

---
+++ modules/settings/index.js
@@ -0,0 +1,3 @@
+angular.module('ma:settings', [])
+  .controller('settings:edit', <% include controllers/edit %>)
+  .controller('settings:show', <% include controllers/show %>);
---
+++ modules/users/index.js
@@ -0,0 +1,3 @@
+angular.module('ma:users', [])
+  .controller('users:edit', <% include controllers/edit %>)
+  .controller('users:show', <% include controllers/show %>);

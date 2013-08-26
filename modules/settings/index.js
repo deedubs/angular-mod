7ff9b4b6472ece4b7b847e23c6158f952da5122b
Dan Williams <me@deedubs.com>
2013-08-26 13:46:01 -0400

    View inlining

--- modules/settings/index.js
+++ modules/settings/index.js
@@ -1,3 +1,11 @@
 angular.module('ma:settings', [])
   .controller('settings:edit', <% include controllers/edit.js %>)
-  .controller('settings:show', <% include controllers/show.js %>);
+  .controller('settings:show', <% include controllers/show.js %>)
+  .config(function ($routeProvider) {
+    
+    $routeProvider
+      .when('/settings', {
+        controller: 'settings:show',
+        template: <%= template('views/show.jade') %>
+      });    
+  })
--- vendor/server.js
+++ vendor/server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express();
 var fs = require('fs');
+var jade = require('jade');
 
 var deps = fs.readdirSync(__dirname + '/../modules')
 
@@ -8,6 +9,12 @@ app.use(express.static('../'));
 
 app.engine('.js', require('ejs').__express);
 
+app.locals.template = function (template) {
+  var paths = this.filename.split('/');
+  paths.pop();
+  
+  return "'" + jade.render(fs.readFileSync(paths.join('/') + "/" + template)) + "'";
+}
 
 app.get('/', function (req, res) {
   res.render('../index.jade', {deps: deps});  
